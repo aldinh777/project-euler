a0f211d13d3abc92ff53f301bd2adc17e511f4a5
aldinh777 <aldi.nh789@gmail.com>
2021-09-05 01:34:59 +0800

    update rust pow

--- c-006/main.rs
+++ c-006/main.rs
@@ -1,10 +1,10 @@
 fn sum_dif_square(num: i32) -> i32 {
     let x: i32 = (1..(num + 1))
-        .map(|a| i32::pow(a, 2))
+        .map(|a| a.pow(2))
         .sum();
     let y: i32 = (1..(num + 1))
         .sum();
-    return i32::pow(y, 2) - x;
+    return y.pow(2) - x;
 }
 
 fn main() {
