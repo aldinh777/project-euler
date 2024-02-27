87aa4d02c5c2d829f5c9130e709499f879712e01
aldinh777 <aldi.nh789@gmail.com>
2024-02-27 11:26:17 +0800

    js solution 28 to 30

---
+++ 03x10/c-030/main.js
@@ -0,0 +1,29 @@
+const POW = 5
+
+function getDigit(num) {
+    const out = []
+    for (let i = num.toString().length - 1; i >= 0; i--) {
+        const pao = 10 ** i
+        const p = num - (num % pao)
+        num -= p
+        out.push(p / pao)
+    }
+    return out
+}
+
+function sumPowDigits(num, pow = POW) {
+    return getDigit(num).reduce((p, n) => (p += n ** pow), 0)
+}
+
+function digitPowerSum() {
+    let sum = 0
+    const max = 9 ** POW * POW
+    for (let i = 2; i < max; i++) {
+        if (sumPowDigits(i) === i) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+console.log(digitPowerSum())
