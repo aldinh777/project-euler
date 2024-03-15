8d9f12a334dda192d9b712cb2db785ff2b5d6459
aldinh777 <aldi.nh789@gmail.com>
2024-03-16 06:08:21 +0800

    better solution for problem 17

--- 02x10/c-017/main.py
+++ 02x10/c-017/main.py
@@ -1,11 +1,56 @@
-from pathlib import Path
-import json
 import re
 
-filepath = Path(__file__).parent.joinpath('numbers.json').absolute()
-numbers = json.load(open(filepath))
+def getBritishVocal(num):
+    nums = {
+        0:  '',
+        1:  'one',
+        2:  'two',
+        3:  'three',
+        4:  'four',
+        5:  'five',
+        6:  'six',
+        7:  'seven',
+        8:  'eight',
+        9:  'nine',
+        10:  'ten',
+        11:  "eleven",
+        12:  "twelve",
+        13:  "thirteen",
+        14:  "fourteen",
+        15:  "fifteen",
+        16:  "sixteen",
+        17:  "seventeen",
+        18:  "eighteen",
+        19:  "nineteen",
+        20:  "twenty",
+        30:  "thirty",
+        40:  "forty",
+        50:  "fifty",
+        60:  "sixty",
+        70:  "seventy",
+        80:  "eighty",
+        90:  "ninety",
+    }
+    if nums.get(num) != None:
+        return nums.get(num)
+    if num > 999:
+        thousandth = int(num / 1000)
+        rest = num - 1000 * thousandth
+        vocalThousandth = getBritishVocal(thousandth) + ' thousand'
+        vocalRest = ' and' + getBritishVocal(rest) if rest > 0 else ''
+        return vocalThousandth + vocalRest
+    if num > 99:
+        hundredth = int(num / 100)
+        rest = num - 100 * hundredth
+        vocalHundred = getBritishVocal(hundredth) + ' hundred'
+        vocalRest = ' and' + getBritishVocal(rest) if rest > 0 else ''
+        return vocalHundred + vocalRest
+    if num > 20:
+        tenth = int(num / 10) * 10
+        onceth = num % 10
+        return getBritishVocal(tenth) + '-' + getBritishVocal(onceth)
 
 def numberLetter():
-    return sum([len(re.sub('[ -]', '', a)) for a in numbers.values()])
+    return sum([len(re.sub('[ -]', '', a)) for a in [getBritishVocal(b) for b in range(1, 1001)]])
 
 print(numberLetter())
--- 02x10/c-017/main.rb
+++ 02x10/c-017/main.rb
@@ -1,11 +1,58 @@
-require 'json'
-
-filepath = File.join(__dir__, 'numbers.json')
-$numbers = JSON.parse(File.read(filepath))
+def getBritishVocal num
+    case num
+        when 0 then return ''
+        when 1 then return 'one'
+        when 2 then return 'two'
+        when 3 then return 'three'
+        when 4 then return 'four'
+        when 5 then return 'five'
+        when 6 then return 'six'
+        when 7 then return 'seven'
+        when 8 then return 'eight'
+        when 9 then return 'nine'
+        when 10 then return 'ten'
+        when 11 then return "eleven"
+        when 12 then return "twelve"
+        when 13 then return "thirteen"
+        when 14 then return "fourteen"
+        when 15 then return "fifteen"
+        when 16 then return "sixteen"
+        when 17 then return "seventeen"
+        when 18 then return "eighteen"
+        when 19 then return "nineteen"
+        when 20 then return "twenty"
+        when 30 then return "thirty"
+        when 40 then return "forty"
+        when 50 then return "fifty"
+        when 60 then return "sixty"
+        when 70 then return "seventy"
+        when 80 then return "eighty"
+        when 90 then return "ninety"
+    end
+    if num > 999 then
+        thousandth = (num / 1000).floor
+        rest = num - 1000 * thousandth
+        vocalThousandth = getBritishVocal(thousandth) + ' thousand'
+        vocalRest = rest > 0 ? ' and' + getBritishVocal(rest) : ''
+        return vocalThousandth + vocalRest
+    end
+    if num > 99 then
+        hundredth = (num / 100).floor
+        rest = num - 100 * hundredth
+        vocalHundred = getBritishVocal(hundredth) + ' hundred'
+        vocalRest = rest > 0 ? ' and' + getBritishVocal(rest) : ''
+        return vocalHundred + vocalRest
+    end
+    if num > 20 then
+        tenth = (num / 10) * 10.floor
+        onceth = num % 10
+        return getBritishVocal(tenth) + '-' + getBritishVocal(onceth)
+    end
+end
 
 def numberLetter
-    $numbers
-        .map { |_, a| a.gsub(/[ -]/, '').length }
+    (1..1000)
+        .map { |a| getBritishVocal(a).gsub(/[ -]/, '').length }
         .sum
 end
 
